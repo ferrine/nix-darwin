3b8303af9d5f1efcb43ae6430f0592ba07f178b2
Max Kochurov <maxim.v.kochurov@gmail.com>
2024-04-21 11:58:56 +0300

    install emacs

---
+++ configuration.nix
@@ -0,0 +1,42 @@
+{ self, pkgs, inputs, ... }: {
+  imports = [ ];
+  nixpkgs = {
+    hostPlatform = "aarch64-darwin";
+    overlays = with inputs; [
+       # 1. use `emacs` overlay provided by this repo
+       darwin-emacs.overlays.emacs
+       # 2. use `package` overlay provided by nix-community/emacs-overlay
+       darwin-emacs-packages.overlays.package
+     ];
+  };
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    with pkgs; [
+      emacs
+    ];
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
+
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  # The platform the configuration will be used on.
+  users.users.ferres = {
+    name = "ferres";
+    home = "/Users/ferres";
+  };
+}
--- flake.nix
+++ flake.nix
@@ -24,58 +24,15 @@
     };
   };
   outputs = inputs@{ self, home-manager, darwin, darwin-emacs, darwin-emacs-packages, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      imports = [ ];
-      nixpkgs = {
-        hostPlatform = "aarch64-darwin";
-        overlays = [
-           # 1. use `emacs` overlay provided by this repo
-           darwin-emacs.overlays.emacs
-           # 2. use `package` overlay provided by nix-community/emacs-overlay
-           darwin-emacs-packages.overlays.package
-         ];
-      };
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        with pkgs; [ 
-          vim
-        ];
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
-
-      # The platform the configuration will be used on.
-      users.users.ferres = {
-        name = "ferres";
-        home = "/Users/ferres";
-      };
-    };
-  in
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#air
-    darwinConfigurations."air" = darwin.lib.darwinSystem {
-      modules = [ configuration ];
+    darwinConfigurations.air = darwin.lib.darwinSystem {
+      modules = [ ./configuration.nix ];
+      specialArgs = {inherit inputs self;};
     };
 
     # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."air".pkgs;
+    darwinPackages = self.darwinConfigurations.air.pkgs;
   };
 }
