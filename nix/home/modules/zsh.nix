d6e74049a25388a3a22115f5ea628c92cf74b562
Max Kochurov <maxim.v.kochurov@gmail.com>
2024-05-11 18:32:49 +0300

    zsh fix for emacs

--- nix/home/modules/zsh.nix
+++ nix/home/modules/zsh.nix
@@ -11,6 +11,13 @@ in
         theme = lib.mkDefault "nebirhos";
         plugins = [ "git" "sudo" ];
       };
+      initExtraFirst = ''
+        if [[ $TERM = dumb ]]; then
+          unsetopt zle
+          PS1='$ '
+          # that's all we need, no extra
+        fi
+      '';
     };
   };
 }
