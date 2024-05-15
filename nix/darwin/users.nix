18e26ef29ff9e12deeccefee07625bc63886644d
Max Kochurov <maxim.v.kochurov@gmail.com>
2024-05-15 19:40:45 +0300

    fix lsregister

--- nix/darwin/users.nix
+++ nix/darwin/users.nix
@@ -12,6 +12,16 @@ in
     home.packages = [
       self.legacyPackages.${pkgs.system}.localPython3Packages.ledger-agent
     ];
+    home.activation.fix-lsregister =
+      let
+        lsregister = "/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister";
+      in
+      ''
+        # unregister all from the previous generation
+        ${lsregister} -dump | /usr/bin/grep /nix/store | /usr/bin/awk '{ print $2 }' | /usr/bin/xargs ${lsregister} -f -u
+        # refresh with new generation
+        /usr/bin/find $(realpath $HOME/Applications/Home\ Manager\ Apps) -name '*.app' | /usr/bin/xargs ${lsregister} -f
+      '';
     imports = [
       {
         stuff.profiles.desktop.enable = true;
