9837a831da023320e82c7db132369978320bddad
Max Kochurov <maxim.v.kochurov@gmail.com>
2024-05-16 13:14:11 +0300

    add flake root to the default shell

--- templates/simple/flake.nix
+++ templates/simple/flake.nix
@@ -4,11 +4,16 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-root = {
+      url = "github:srid/flake-root";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ flake-parts, flake-root, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
+	flake-root.flakeModule
         ./nix
       ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
--- templates/simple/nix/shell.nix
+++ templates/simple/nix/shell.nix
@@ -1,11 +1,9 @@
 { ... } @flake:
 {
-  perSystem = { pkgs, ... } @localFlake:
-    let
-      # inherit (self'.legacyPackages);
-    in
+  perSystem = { pkgs, config, ... } @localFlake:
     {
-      devShells.default = pkgs.mkShell rec {
+      devShells.default = pkgs.mkShell {
+	inputsFrom = [ config.flake-root.devShell ];
         name = "default shell";
         packages = with pkgs; [ nixpkgs-fmt ];
       };
