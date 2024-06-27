f3f3f1dfea5e2603f4c22daacd277e4b4057b9db
ferres <justferres@yandex.ru>
2024-06-28 00:40:30 +0300

    move sources to third party ad it seems to be a better name now for the entry point

--- default.nix
+++ default.nix
@@ -1,18 +1,11 @@
-{ sources ? import ./nix/sources.nix }: # import the sources
+{ }: # import the sources
 let
-  pkgs = import sources.nixpkgs {
-    config = { allowUnfree = true; };
-  };
-  inherit (pkgs) lib;
+  readTree = import ./nix/kit/readTree {};
+  fix = f: (let x = f x; in x);
 in
-lib.makeScope pkgs.newScope (self: {
-  inherit pkgs lib sources;
-  third-party = lib.packagesFromDirectoryRecursive {
-    inherit (self) callPackage;
-    directory = ./third_party;
-  };
-  repo = lib.packagesFromDirectoryRecursive {
-    inherit (self) callPackage;
-    directory = ./src;
-  };
-})
+  fix (self: readTree {
+    path = ./.;
+    args = {
+      universe = self;
+    };
+  })
